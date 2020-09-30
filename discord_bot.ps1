97f7c1de0efe747f48027210b8d5606123ed01d5
Vanisher <vanisher314@gmail.com>
2020-09-30 08:53:14 -0600

    fixed bug
    powershell script was wouldn't run because i had a quote in the wrong place

--- discord_bot.ps1
+++ discord_bot.ps1
@@ -1,6 +1,6 @@
 Set-Location D:\Desktop\source_code\GitHub\discord_bot\blank_files
 
-$ValueList='Beep','Boop','Obi-Wan, you're my only hope','Prepare to be terminated','ZZzzzZZzZzzzZZTttTTt','kablamo','pew pew','vvvvt vrrrr whiiiiir','powering up'
+$ValueList='Beep','Boop',"Obi-Wan, you're my only hope",'Prepare to be terminated','ZZzzzZZzZzzzZZTttTTt','kablamo','pew pew','vvvvt vrrrr whiiiiir','powering up'
 
 New-Item -Name "$(Get-Date -Format "MM-dd-yyyy_hh-mm-ss.fff")" -Value "$(Get-Random -InputObject $ValueList)" -Path D:\Desktop\source_code\GitHub\discord_bot\blank_files
 
