3a1e7e9ef358ef3ac4693b83bffdfb3c3cc48f8f
Vanisher <vanisher314@gmail.com>
2020-09-30 08:47:28 -0600

    created activity generator ;)

---
+++ discord_bot.ps1
@@ -0,0 +1,10 @@
+Set-Location D:\Desktop\source_code\GitHub\discord_bot\blank_files
+
+$ValueList='Beep','Boop','Obi-Wan, you're my only hope','Prepare to be terminated','ZZzzzZZzZzzzZZTttTTt','kablamo','pew pew','vvvvt vrrrr whiiiiir','powering up'
+
+New-Item -Name "$(Get-Date -Format "MM-dd-yyyy_hh-mm-ss.fff")" -Value "$(Get-Random -InputObject $ValueList)" -Path D:\Desktop\source_code\GitHub\discord_bot\blank_files
+
+git fetch
+git add .
+git commit -m "blank update"
+git push
